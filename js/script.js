dbf7cef0e813dfc79cb99dd1153b6f7c84598d9c
OliverGottlieb <olivergottlieb1@protonmail.com>
2026-02-25 12:09:32 +0100

    second commit

--- js/script.js
+++ js/script.js
@@ -10,5 +10,33 @@ headingOne.style.fontSize = "3rem";
 headingOne.style.color = "Maroon";
 
 const paragraphs = document.querySelectorAll("p");
-paragraphs.style.fontSize = "2rem";
-paragraphs.style.fontWeight = "700";
+
+paragraphs.forEach(function (paragraph) {
+  paragraph.style.fontSize = "2rem";
+  paragraph.style.fontWeight = "700";
+});
+
+
+let listItemOne = document.querySelector("#list-item-one");
+listItemOne.style.fontSize = "1.2rem";
+listItemOne.style.listStyleType = "circle";
+
+
+let listItemTwo = document.querySelector("#list-item-two");
+listItemTwo.style.fontSize = "1.2rem";
+listItemTwo.style.listStyleType = "disc";
+
+let listItemThree = document.querySelector("#list-item-three");
+listItemThree.style.fontSize = "1.2rem";
+listItemThree.style.listStyleType = "square";
+
+
+const ul = document.querySelector("ul");
+ul.style.fontSize = "22px";
+
+
+
+
+
+
+
