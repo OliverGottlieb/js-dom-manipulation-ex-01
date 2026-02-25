a66121f1d638706dd81cfab40a1d312be7892a71
OliverGottlieb <olivergottlieb1@protonmail.com>
2026-02-25 11:42:00 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const websiteBody = document.body;
+
+const headingOne = document.querySelector("h1");
+
+websiteBody.style.background = "yellow";
+
+headingOne.style.fontSize = "3rem";
+headingOne.style.color = "Maroon";
+
+const paragraphs = document.querySelectorAll("p");
+paragraphs.style.fontSize = "2rem";
+paragraphs.style.fontWeight = "700";
